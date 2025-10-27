8a24d0c7508eff3db80881c55bfdcd5624c10847
Mohamed Aarif <mohameda@ane.ae>
2025-10-27 17:29:59 +0400

    Infra setup

---
+++ infra/terraform-azure/modules/network/variables.tf
@@ -0,0 +1,6 @@
+variable "name" { type = string }
+variable "rg_name" { type = string }
+variable "location" { type = string }
+variable "address_space" { type = list(string) default = ["10.10.0.0/16"] }
+variable "subnet_aks" { type = string default = "10.10.1.0/24" }
+variable "subnet_data" { type = string default = "10.10.2.0/24" }

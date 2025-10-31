9243717e1993513b432a47fcee9dfc51fb73c866
Sarvesht123 <151932457+Sarvesht123@users.noreply.github.com>
2025-10-31 13:19:26 +0400

    Update providers.tf

--- infra/terraform-azure/envs/dev/providers.tf
+++ infra/terraform-azure/envs/dev/providers.tf
@@ -1,7 +1,21 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    azurerm   = { source = "hashicorp/azurerm" version = "~> 3.112" }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.112"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "RG-DIGIHEALTH"
+    storage_account_name = "terraformdigicodeops"   # change
+    container_name       = "terraformcontainer"
+    key                  = "dev.tfstate"
+    # When using access key from env (ARM_ACCESS_KEY), do not set use_azuread_auth
   }
 }
-provider "azurerm" { features {} }
+
+provider "azurerm" {
+  features {}
+}

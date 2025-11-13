ef296d69fe772b7963c10bdf0a47fdcfcf54ade4
mohamed-aarif <mohamed.aarif@live.com>
2025-11-13 14:12:13 +0400

    Ensure identity service resolves configured connection string

--- src/services/identity/IdentityService.EntityFrameworkCore/IdentityServiceEntityFrameworkCoreModule.cs
+++ src/services/identity/IdentityService.EntityFrameworkCore/IdentityServiceEntityFrameworkCoreModule.cs
@@ -1,5 +1,9 @@
+using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.Data;
 using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore.PostgreSql;
 using Volo.Abp.Modularity;
@@ -14,16 +18,41 @@ public class IdentityServiceEntityFrameworkCoreModule : AbpModule
 {
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
+        var configuration = context.Services.GetConfiguration();
+
         context.Services.AddAbpDbContext<IdentityServiceDbContext>(options =>
         {
             options.AddDefaultRepositories(includeAllEntities: true);
+        });
+
+        Configure<AbpDbConnectionOptions>(options =>
+        {
+            var connectionString = configuration.GetConnectionString(IdentityServiceDbProperties.ConnectionStringName)
+                ?? configuration.GetConnectionString("IdentityService");
+
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException(
+                    $"Connection string '{IdentityServiceDbProperties.ConnectionStringName}' was not found in configuration.");
+            }
+
+            options.ConnectionStrings.Default = connectionString;
+            options.ConnectionStrings[IdentityServiceDbProperties.ConnectionStringName] = connectionString;
         });
 
         Configure<AbpDbContextOptions>(options =>
         {
             options.Configure(configurationContext =>
             {
-                configurationContext.DbContextOptions.UseNpgsql();
+                var connectionString = configurationContext.ConnectionString;
+
+                if (string.IsNullOrWhiteSpace(connectionString))
+                {
+                    throw new InvalidOperationException(
+                        "The database connection string could not be resolved for IdentityServiceDbContext.");
+                }
+
+                configurationContext.DbContextOptions.UseNpgsql(connectionString);
             });
         });
     }

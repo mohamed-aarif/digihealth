a284ca07a52e46872ec8fae55071e3d7043d65bb
mohamed-aarif <mohamed.aarif@live.com>
2025-11-16 16:38:44 +0400

    Fix identity contracts module dependency

--- src/services/identity/IdentityService.Application.Contracts/IdentityServiceApplicationContractsModule.cs
+++ src/services/identity/IdentityService.Application.Contracts/IdentityServiceApplicationContractsModule.cs
@@ -1,12 +1,11 @@
 using Volo.Abp.Application;
-using Volo.Abp.Ddd.Application;
 using Volo.Abp.Modularity;
 
 namespace IdentityService;
 
 [DependsOn(
     typeof(IdentityServiceDomainSharedModule),
-    typeof(AbpDddApplicationModule)
+    typeof(AbpDddApplicationContractsModule)
 )]
 public class IdentityServiceApplicationContractsModule : AbpModule
 {

9f3cab3cb7c535ff06c544e6019f89c88ef08bba
mohamed-aarif <mohamed.aarif@live.com>
2025-11-18 07:59:47 +0400

    Wrap identity schema migrator in a unit of work

--- src/services/identity/IdentityService.EntityFrameworkCore/EntityFrameworkCore/EntityFrameworkCoreIdentityServiceDbSchemaMigrator.cs
+++ src/services/identity/IdentityService.EntityFrameworkCore/EntityFrameworkCore/EntityFrameworkCoreIdentityServiceDbSchemaMigrator.cs
@@ -3,22 +3,30 @@ using IdentityService.Data;
 using Microsoft.EntityFrameworkCore;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.Uow;
 
 namespace IdentityService.EntityFrameworkCore;
 
 public class EntityFrameworkCoreIdentityServiceDbSchemaMigrator : IIdentityServiceDbSchemaMigrator, ITransientDependency
 {
     private readonly IDbContextProvider<IdentityServiceDbContext> _dbContextProvider;
+    private readonly IUnitOfWorkManager _unitOfWorkManager;
 
     public EntityFrameworkCoreIdentityServiceDbSchemaMigrator(
-        IDbContextProvider<IdentityServiceDbContext> dbContextProvider)
+        IDbContextProvider<IdentityServiceDbContext> dbContextProvider,
+        IUnitOfWorkManager unitOfWorkManager)
     {
         _dbContextProvider = dbContextProvider;
+        _unitOfWorkManager = unitOfWorkManager;
     }
 
     public async Task MigrateAsync()
     {
+        await using var uow = _unitOfWorkManager.Begin(requiresNew: true);
+
         var dbContext = await _dbContextProvider.GetDbContextAsync();
         await dbContext.Database.MigrateAsync();
+
+        await uow.CompleteAsync();
     }
 }

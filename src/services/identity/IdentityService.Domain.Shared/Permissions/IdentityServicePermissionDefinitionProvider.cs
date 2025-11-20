59de00f500d88150be2ef905e3aee049e5cffa41
mohamed-aarif <mohamed.aarif@live.com>
2025-11-20 13:59:58 +0400

    Guard duplicate permission definitions

--- src/services/identity/IdentityService.Domain.Shared/Permissions/IdentityServicePermissionDefinitionProvider.cs
+++ src/services/identity/IdentityService.Domain.Shared/Permissions/IdentityServicePermissionDefinitionProvider.cs
@@ -11,13 +11,24 @@ public class IdentityServicePermissionDefinitionProvider : PermissionDefinitionP
         var group = context.GetGroupOrNull(IdentityServicePermissions.GroupName)
                     ?? context.AddGroup(IdentityServicePermissions.GroupName, L("Permission:IdentityService"));
 
-        var doctors = group.AddPermission(IdentityServicePermissions.Doctors.Default, L("Permission:Doctors"));
-        doctors.AddChild(IdentityServicePermissions.Doctors.Manage, L("Permission:Doctors.Manage"));
+        var doctors = group.GetPermissionOrNull(IdentityServicePermissions.Doctors.Default)
+                      ?? group.AddPermission(IdentityServicePermissions.Doctors.Default, L("Permission:Doctors"));
+        if (doctors.GetChild(IdentityServicePermissions.Doctors.Manage) == null)
+        {
+            doctors.AddChild(IdentityServicePermissions.Doctors.Manage, L("Permission:Doctors.Manage"));
+        }
 
-        var patients = group.AddPermission(IdentityServicePermissions.Patients.Default, L("Permission:Patients"));
-        patients.AddChild(IdentityServicePermissions.Patients.Manage, L("Permission:Patients.Manage"));
+        var patients = group.GetPermissionOrNull(IdentityServicePermissions.Patients.Default)
+                       ?? group.AddPermission(IdentityServicePermissions.Patients.Default, L("Permission:Patients"));
+        if (patients.GetChild(IdentityServicePermissions.Patients.Manage) == null)
+        {
+            patients.AddChild(IdentityServicePermissions.Patients.Manage, L("Permission:Patients.Manage"));
+        }
 
-        group.AddPermission(IdentityServicePermissions.Profile.Default, L("Permission:Profile"));
+        if (group.GetPermissionOrNull(IdentityServicePermissions.Profile.Default) == null)
+        {
+            group.AddPermission(IdentityServicePermissions.Profile.Default, L("Permission:Profile"));
+        }
     }
 
     private static LocalizableString L(string name)

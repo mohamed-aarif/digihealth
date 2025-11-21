ba408a2e955bdceb916667695239857c64d5867f
mohamed-aarif <mohamed.aarif@live.com>
2025-11-21 12:41:03 +0400

    Ensure family link tenant id serialized

--- src/services/identity/IdentityService.Application.Contracts/FamilyLinks/Dtos/FamilyLinkDto.cs
+++ src/services/identity/IdentityService.Application.Contracts/FamilyLinks/Dtos/FamilyLinkDto.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Text.Json.Serialization;
 using Volo.Abp.Application.Dtos;
 
 namespace IdentityService.FamilyLinks.Dtos;
 
 public class FamilyLinkDto : FullAuditedEntityDto<Guid>
 {
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
     public Guid? TenantId { get; set; }
     public Guid PatientId { get; set; }
     public Guid FamilyUserId { get; set; }

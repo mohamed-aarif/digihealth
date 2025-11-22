873dac24e15c67511f3600c0f64cba1c6c17aee4
mohamed-aarif <mohamed.aarif@live.com>
2025-11-22 21:48:22 +0400

    Use basic auth for Swagger authorization code flow

--- src/digihealth.HttpApi.Host/digihealthHttpApiHostModule.cs
+++ src/digihealth.HttpApi.Host/digihealthHttpApiHostModule.cs
@@ -215,6 +215,7 @@ public class digihealthHttpApiHostModule : AbpModule
             var configuration = context.ServiceProvider.GetRequiredService<IConfiguration>();
             c.OAuthClientId(configuration["AuthServer:SwaggerClientId"]);
             c.OAuthClientSecret("digihealth_Swagger_DevSecret_123!");
+            c.OAuthUseBasicAuthenticationWithAccessCodeGrant();
             c.OAuthScopes("digihealth");
         });
 

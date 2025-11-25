22dd003625d579da5926a0c951018ad78f64b99d
mohamed-aarif <mohamed.aarif@live.com>
2025-11-25 21:15:12 +0400

    Add missing dictionary namespace for patient host swagger config

--- src/services/patient/PatientService.HttpApi.Host/PatientServiceHttpApiHostModule.cs
+++ src/services/patient/PatientService.HttpApi.Host/PatientServiceHttpApiHostModule.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;

4cf0ad727c6a52d7647fef89c664e851611ae6f6
mohamed-aarif <mohamed.aarif@live.com>
2025-12-03 09:22:01 +0400

    Fix configuration permissions namespace

--- src/services/configuration/ConfigurationService.Domain.Shared/Permissions/ConfigurationPermissionDefinitionProvider.cs
+++ src/services/configuration/ConfigurationService.Domain.Shared/Permissions/ConfigurationPermissionDefinitionProvider.cs
@@ -1,49 +1,50 @@
 using DigiHealth.ConfigurationService.Localization;
 using Volo.Abp.Authorization.Permissions;
-using Volo.Abp.Localization;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Localization;
 
-namespace DigiHealth.ConfigurationService.Permissions;
-
-public class ConfigurationPermissionDefinitionProvider : PermissionDefinitionProvider, ITransientDependency
+namespace DigiHealth.ConfigurationService.Permissions
 {
-    public override void Define(IPermissionDefinitionContext context)
+    public class ConfigurationPermissionDefinitionProvider : PermissionDefinitionProvider, ITransientDependency
     {
-        var group = context.AddGroup(ConfigurationPermissions.GroupName, L("Permission:Configuration"));
+        public override void Define(IPermissionDefinitionContext context)
+        {
+            var group = context.AddGroup(ConfigurationPermissions.GroupName, L("Permission:Configuration"));
 
-        var appointmentStatuses = group.AddPermission(ConfigurationPermissions.AppointmentStatuses.Default, L("Permission:AppointmentStatuses"));
-        appointmentStatuses.AddChild(ConfigurationPermissions.AppointmentStatuses.Manage, L("Permission:AppointmentStatuses.Manage"));
+            var appointmentStatuses = group.AddPermission(ConfigurationPermissions.AppointmentStatuses.Default, L("Permission:AppointmentStatuses"));
+            appointmentStatuses.AddChild(ConfigurationPermissions.AppointmentStatuses.Manage, L("Permission:AppointmentStatuses.Manage"));
 
-        var appointmentChannels = group.AddPermission(ConfigurationPermissions.AppointmentChannels.Default, L("Permission:AppointmentChannels"));
-        appointmentChannels.AddChild(ConfigurationPermissions.AppointmentChannels.Manage, L("Permission:AppointmentChannels.Manage"));
+            var appointmentChannels = group.AddPermission(ConfigurationPermissions.AppointmentChannels.Default, L("Permission:AppointmentChannels"));
+            appointmentChannels.AddChild(ConfigurationPermissions.AppointmentChannels.Manage, L("Permission:AppointmentChannels.Manage"));
 
-        var consentPartyTypes = group.AddPermission(ConfigurationPermissions.ConsentPartyTypes.Default, L("Permission:ConsentPartyTypes"));
-        consentPartyTypes.AddChild(ConfigurationPermissions.ConsentPartyTypes.Manage, L("Permission:ConsentPartyTypes.Manage"));
+            var consentPartyTypes = group.AddPermission(ConfigurationPermissions.ConsentPartyTypes.Default, L("Permission:ConsentPartyTypes"));
+            consentPartyTypes.AddChild(ConfigurationPermissions.ConsentPartyTypes.Manage, L("Permission:ConsentPartyTypes.Manage"));
 
-        var consentStatuses = group.AddPermission(ConfigurationPermissions.ConsentStatuses.Default, L("Permission:ConsentStatuses"));
-        consentStatuses.AddChild(ConfigurationPermissions.ConsentStatuses.Manage, L("Permission:ConsentStatuses.Manage"));
+            var consentStatuses = group.AddPermission(ConfigurationPermissions.ConsentStatuses.Default, L("Permission:ConsentStatuses"));
+            consentStatuses.AddChild(ConfigurationPermissions.ConsentStatuses.Manage, L("Permission:ConsentStatuses.Manage"));
 
-        var daysOfWeek = group.AddPermission(ConfigurationPermissions.DaysOfWeek.Default, L("Permission:DaysOfWeek"));
-        daysOfWeek.AddChild(ConfigurationPermissions.DaysOfWeek.Manage, L("Permission:DaysOfWeek.Manage"));
+            var daysOfWeek = group.AddPermission(ConfigurationPermissions.DaysOfWeek.Default, L("Permission:DaysOfWeek"));
+            daysOfWeek.AddChild(ConfigurationPermissions.DaysOfWeek.Manage, L("Permission:DaysOfWeek.Manage"));
 
-        var deviceTypes = group.AddPermission(ConfigurationPermissions.DeviceTypes.Default, L("Permission:DeviceTypes"));
-        deviceTypes.AddChild(ConfigurationPermissions.DeviceTypes.Manage, L("Permission:DeviceTypes.Manage"));
+            var deviceTypes = group.AddPermission(ConfigurationPermissions.DeviceTypes.Default, L("Permission:DeviceTypes"));
+            deviceTypes.AddChild(ConfigurationPermissions.DeviceTypes.Manage, L("Permission:DeviceTypes.Manage"));
 
-        var medicationIntakeStatuses = group.AddPermission(ConfigurationPermissions.MedicationIntakeStatuses.Default, L("Permission:MedicationIntakeStatuses"));
-        medicationIntakeStatuses.AddChild(ConfigurationPermissions.MedicationIntakeStatuses.Manage, L("Permission:MedicationIntakeStatuses.Manage"));
+            var medicationIntakeStatuses = group.AddPermission(ConfigurationPermissions.MedicationIntakeStatuses.Default, L("Permission:MedicationIntakeStatuses"));
+            medicationIntakeStatuses.AddChild(ConfigurationPermissions.MedicationIntakeStatuses.Manage, L("Permission:MedicationIntakeStatuses.Manage"));
 
-        var notificationChannels = group.AddPermission(ConfigurationPermissions.NotificationChannels.Default, L("Permission:NotificationChannels"));
-        notificationChannels.AddChild(ConfigurationPermissions.NotificationChannels.Manage, L("Permission:NotificationChannels.Manage"));
+            var notificationChannels = group.AddPermission(ConfigurationPermissions.NotificationChannels.Default, L("Permission:NotificationChannels"));
+            notificationChannels.AddChild(ConfigurationPermissions.NotificationChannels.Manage, L("Permission:NotificationChannels.Manage"));
 
-        var notificationStatuses = group.AddPermission(ConfigurationPermissions.NotificationStatuses.Default, L("Permission:NotificationStatuses"));
-        notificationStatuses.AddChild(ConfigurationPermissions.NotificationStatuses.Manage, L("Permission:NotificationStatuses.Manage"));
+            var notificationStatuses = group.AddPermission(ConfigurationPermissions.NotificationStatuses.Default, L("Permission:NotificationStatuses"));
+            notificationStatuses.AddChild(ConfigurationPermissions.NotificationStatuses.Manage, L("Permission:NotificationStatuses.Manage"));
 
-        var vaultRecordTypes = group.AddPermission(ConfigurationPermissions.VaultRecordTypes.Default, L("Permission:VaultRecordTypes"));
-        vaultRecordTypes.AddChild(ConfigurationPermissions.VaultRecordTypes.Manage, L("Permission:VaultRecordTypes.Manage"));
-    }
+            var vaultRecordTypes = group.AddPermission(ConfigurationPermissions.VaultRecordTypes.Default, L("Permission:VaultRecordTypes"));
+            vaultRecordTypes.AddChild(ConfigurationPermissions.VaultRecordTypes.Manage, L("Permission:VaultRecordTypes.Manage"));
+        }
 
-    private static LocalizableString L(string name)
-    {
-        return LocalizableString.Create<ConfigurationServiceResource>(name);
+        private static LocalizableString L(string name)
+        {
+            return LocalizableString.Create<ConfigurationServiceResource>(name);
+        }
     }
 }

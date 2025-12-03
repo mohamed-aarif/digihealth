0e7f5563dad2cd0cdedc2634a835e0f28017f178
mohamed-aarif <mohamed.aarif@live.com>
2025-12-03 20:47:45 +0400

    Make permission definitions idempotent

--- src/services/configuration/ConfigurationService.Domain.Shared/Permissions/ConfigurationPermissionDefinitionProvider.cs
+++ src/services/configuration/ConfigurationService.Domain.Shared/Permissions/ConfigurationPermissionDefinitionProvider.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using DigiHealth.ConfigurationService.Localization;
 using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.DependencyInjection;
@@ -12,35 +13,55 @@ namespace DigiHealth.ConfigurationService.Permissions
             var group = context.GetGroupOrNull(ConfigurationPermissions.GroupName)
                         ?? context.AddGroup(ConfigurationPermissions.GroupName, L("Permission:Configuration"));
 
-            var appointmentStatuses = group.AddPermission(ConfigurationPermissions.AppointmentStatuses.Default, L("Permission:AppointmentStatuses"));
-            appointmentStatuses.AddChild(ConfigurationPermissions.AppointmentStatuses.Manage, L("Permission:AppointmentStatuses.Manage"));
+            var appointmentStatuses = group.GetPermissionOrNull(ConfigurationPermissions.AppointmentStatuses.Default)
+                                   ?? group.AddPermission(ConfigurationPermissions.AppointmentStatuses.Default, L("Permission:AppointmentStatuses"));
+            _ = appointmentStatuses.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.AppointmentStatuses.Manage)
+                ?? appointmentStatuses.AddChild(ConfigurationPermissions.AppointmentStatuses.Manage, L("Permission:AppointmentStatuses.Manage"));
 
-            var appointmentChannels = group.AddPermission(ConfigurationPermissions.AppointmentChannels.Default, L("Permission:AppointmentChannels"));
-            appointmentChannels.AddChild(ConfigurationPermissions.AppointmentChannels.Manage, L("Permission:AppointmentChannels.Manage"));
+            var appointmentChannels = group.GetPermissionOrNull(ConfigurationPermissions.AppointmentChannels.Default)
+                                     ?? group.AddPermission(ConfigurationPermissions.AppointmentChannels.Default, L("Permission:AppointmentChannels"));
+            _ = appointmentChannels.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.AppointmentChannels.Manage)
+                ?? appointmentChannels.AddChild(ConfigurationPermissions.AppointmentChannels.Manage, L("Permission:AppointmentChannels.Manage"));
 
-            var consentPartyTypes = group.AddPermission(ConfigurationPermissions.ConsentPartyTypes.Default, L("Permission:ConsentPartyTypes"));
-            consentPartyTypes.AddChild(ConfigurationPermissions.ConsentPartyTypes.Manage, L("Permission:ConsentPartyTypes.Manage"));
+            var consentPartyTypes = group.GetPermissionOrNull(ConfigurationPermissions.ConsentPartyTypes.Default)
+                                    ?? group.AddPermission(ConfigurationPermissions.ConsentPartyTypes.Default, L("Permission:ConsentPartyTypes"));
+            _ = consentPartyTypes.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.ConsentPartyTypes.Manage)
+                ?? consentPartyTypes.AddChild(ConfigurationPermissions.ConsentPartyTypes.Manage, L("Permission:ConsentPartyTypes.Manage"));
 
-            var consentStatuses = group.AddPermission(ConfigurationPermissions.ConsentStatuses.Default, L("Permission:ConsentStatuses"));
-            consentStatuses.AddChild(ConfigurationPermissions.ConsentStatuses.Manage, L("Permission:ConsentStatuses.Manage"));
+            var consentStatuses = group.GetPermissionOrNull(ConfigurationPermissions.ConsentStatuses.Default)
+                                  ?? group.AddPermission(ConfigurationPermissions.ConsentStatuses.Default, L("Permission:ConsentStatuses"));
+            _ = consentStatuses.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.ConsentStatuses.Manage)
+                ?? consentStatuses.AddChild(ConfigurationPermissions.ConsentStatuses.Manage, L("Permission:ConsentStatuses.Manage"));
 
-            var daysOfWeek = group.AddPermission(ConfigurationPermissions.DaysOfWeek.Default, L("Permission:DaysOfWeek"));
-            daysOfWeek.AddChild(ConfigurationPermissions.DaysOfWeek.Manage, L("Permission:DaysOfWeek.Manage"));
+            var daysOfWeek = group.GetPermissionOrNull(ConfigurationPermissions.DaysOfWeek.Default)
+                              ?? group.AddPermission(ConfigurationPermissions.DaysOfWeek.Default, L("Permission:DaysOfWeek"));
+            _ = daysOfWeek.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.DaysOfWeek.Manage)
+                ?? daysOfWeek.AddChild(ConfigurationPermissions.DaysOfWeek.Manage, L("Permission:DaysOfWeek.Manage"));
 
-            var deviceTypes = group.AddPermission(ConfigurationPermissions.DeviceTypes.Default, L("Permission:DeviceTypes"));
-            deviceTypes.AddChild(ConfigurationPermissions.DeviceTypes.Manage, L("Permission:DeviceTypes.Manage"));
+            var deviceTypes = group.GetPermissionOrNull(ConfigurationPermissions.DeviceTypes.Default)
+                              ?? group.AddPermission(ConfigurationPermissions.DeviceTypes.Default, L("Permission:DeviceTypes"));
+            _ = deviceTypes.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.DeviceTypes.Manage)
+                ?? deviceTypes.AddChild(ConfigurationPermissions.DeviceTypes.Manage, L("Permission:DeviceTypes.Manage"));
 
-            var medicationIntakeStatuses = group.AddPermission(ConfigurationPermissions.MedicationIntakeStatuses.Default, L("Permission:MedicationIntakeStatuses"));
-            medicationIntakeStatuses.AddChild(ConfigurationPermissions.MedicationIntakeStatuses.Manage, L("Permission:MedicationIntakeStatuses.Manage"));
+            var medicationIntakeStatuses = group.GetPermissionOrNull(ConfigurationPermissions.MedicationIntakeStatuses.Default)
+                                          ?? group.AddPermission(ConfigurationPermissions.MedicationIntakeStatuses.Default, L("Permission:MedicationIntakeStatuses"));
+            _ = medicationIntakeStatuses.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.MedicationIntakeStatuses.Manage)
+                ?? medicationIntakeStatuses.AddChild(ConfigurationPermissions.MedicationIntakeStatuses.Manage, L("Permission:MedicationIntakeStatuses.Manage"));
 
-            var notificationChannels = group.AddPermission(ConfigurationPermissions.NotificationChannels.Default, L("Permission:NotificationChannels"));
-            notificationChannels.AddChild(ConfigurationPermissions.NotificationChannels.Manage, L("Permission:NotificationChannels.Manage"));
+            var notificationChannels = group.GetPermissionOrNull(ConfigurationPermissions.NotificationChannels.Default)
+                                        ?? group.AddPermission(ConfigurationPermissions.NotificationChannels.Default, L("Permission:NotificationChannels"));
+            _ = notificationChannels.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.NotificationChannels.Manage)
+                ?? notificationChannels.AddChild(ConfigurationPermissions.NotificationChannels.Manage, L("Permission:NotificationChannels.Manage"));
 
-            var notificationStatuses = group.AddPermission(ConfigurationPermissions.NotificationStatuses.Default, L("Permission:NotificationStatuses"));
-            notificationStatuses.AddChild(ConfigurationPermissions.NotificationStatuses.Manage, L("Permission:NotificationStatuses.Manage"));
+            var notificationStatuses = group.GetPermissionOrNull(ConfigurationPermissions.NotificationStatuses.Default)
+                                        ?? group.AddPermission(ConfigurationPermissions.NotificationStatuses.Default, L("Permission:NotificationStatuses"));
+            _ = notificationStatuses.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.NotificationStatuses.Manage)
+                ?? notificationStatuses.AddChild(ConfigurationPermissions.NotificationStatuses.Manage, L("Permission:NotificationStatuses.Manage"));
 
-            var vaultRecordTypes = group.AddPermission(ConfigurationPermissions.VaultRecordTypes.Default, L("Permission:VaultRecordTypes"));
-            vaultRecordTypes.AddChild(ConfigurationPermissions.VaultRecordTypes.Manage, L("Permission:VaultRecordTypes.Manage"));
+            var vaultRecordTypes = group.GetPermissionOrNull(ConfigurationPermissions.VaultRecordTypes.Default)
+                                   ?? group.AddPermission(ConfigurationPermissions.VaultRecordTypes.Default, L("Permission:VaultRecordTypes"));
+            _ = vaultRecordTypes.Children.FirstOrDefault(child => child.Name == ConfigurationPermissions.VaultRecordTypes.Manage)
+                ?? vaultRecordTypes.AddChild(ConfigurationPermissions.VaultRecordTypes.Manage, L("Permission:VaultRecordTypes.Manage"));
         }
 
         private static LocalizableString L(string name)
--- src/services/patient/PatientService.Domain.Shared/Permissions/PatientServicePermissionDefinitionProvider.cs
+++ src/services/patient/PatientService.Domain.Shared/Permissions/PatientServicePermissionDefinitionProvider.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using PatientService.Localization;
 using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Localization;
@@ -12,23 +13,37 @@ public class PatientServicePermissionDefinitionProvider : PermissionDefinitionPr
         var patientGroup = context.GetGroupOrNull(PatientServicePermissions.GroupName)
                           ?? context.AddGroup(PatientServicePermissions.GroupName, L("Permission:PatientService"));
 
-        var profiles = patientGroup.AddPermission(PatientServicePermissions.PatientProfiles.Default, L("Permission:PatientProfiles"));
-        profiles.AddChild(PatientServicePermissions.PatientProfiles.Create, L("Permission:Create"));
-        profiles.AddChild(PatientServicePermissions.PatientProfiles.Update, L("Permission:Update"));
-        profiles.AddChild(PatientServicePermissions.PatientProfiles.Delete, L("Permission:Delete"));
+        var profiles = patientGroup.GetPermissionOrNull(PatientServicePermissions.PatientProfiles.Default)
+                      ?? patientGroup.AddPermission(PatientServicePermissions.PatientProfiles.Default, L("Permission:PatientProfiles"));
+        _ = profiles.Children.FirstOrDefault(child => child.Name == PatientServicePermissions.PatientProfiles.Create)
+            ?? profiles.AddChild(PatientServicePermissions.PatientProfiles.Create, L("Permission:Create"));
+        _ = profiles.Children.FirstOrDefault(child => child.Name == PatientServicePermissions.PatientProfiles.Update)
+            ?? profiles.AddChild(PatientServicePermissions.PatientProfiles.Update, L("Permission:Update"));
+        _ = profiles.Children.FirstOrDefault(child => child.Name == PatientServicePermissions.PatientProfiles.Delete)
+            ?? profiles.AddChild(PatientServicePermissions.PatientProfiles.Delete, L("Permission:Delete"));
 
-        var summaries = patientGroup.AddPermission(PatientServicePermissions.PatientMedicalSummaries.Default, L("Permission:PatientMedicalSummaries"));
-        summaries.AddChild(PatientServicePermissions.PatientMedicalSummaries.Create, L("Permission:Create"));
-        summaries.AddChild(PatientServicePermissions.PatientMedicalSummaries.Update, L("Permission:Update"));
-        summaries.AddChild(PatientServicePermissions.PatientMedicalSummaries.Delete, L("Permission:Delete"));
+        var summaries = patientGroup.GetPermissionOrNull(PatientServicePermissions.PatientMedicalSummaries.Default)
+                       ?? patientGroup.AddPermission(PatientServicePermissions.PatientMedicalSummaries.Default, L("Permission:PatientMedicalSummaries"));
+        _ = summaries.Children.FirstOrDefault(child => child.Name == PatientServicePermissions.PatientMedicalSummaries.Create)
+            ?? summaries.AddChild(PatientServicePermissions.PatientMedicalSummaries.Create, L("Permission:Create"));
+        _ = summaries.Children.FirstOrDefault(child => child.Name == PatientServicePermissions.PatientMedicalSummaries.Update)
+            ?? summaries.AddChild(PatientServicePermissions.PatientMedicalSummaries.Update, L("Permission:Update"));
+        _ = summaries.Children.FirstOrDefault(child => child.Name == PatientServicePermissions.PatientMedicalSummaries.Delete)
+            ?? summaries.AddChild(PatientServicePermissions.PatientMedicalSummaries.Delete, L("Permission:Delete"));
 
-        var links = patientGroup.AddPermission(PatientServicePermissions.PatientExternalLinks.Default, L("Permission:PatientExternalLinks"));
-        links.AddChild(PatientServicePermissions.PatientExternalLinks.Create, L("Permission:Create"));
-        links.AddChild(PatientServicePermissions.PatientExternalLinks.Update, L("Permission:Update"));
-        links.AddChild(PatientServicePermissions.PatientExternalLinks.Delete, L("Permission:Delete"));
+        var links = patientGroup.GetPermissionOrNull(PatientServicePermissions.PatientExternalLinks.Default)
+                   ?? patientGroup.AddPermission(PatientServicePermissions.PatientExternalLinks.Default, L("Permission:PatientExternalLinks"));
+        _ = links.Children.FirstOrDefault(child => child.Name == PatientServicePermissions.PatientExternalLinks.Create)
+            ?? links.AddChild(PatientServicePermissions.PatientExternalLinks.Create, L("Permission:Create"));
+        _ = links.Children.FirstOrDefault(child => child.Name == PatientServicePermissions.PatientExternalLinks.Update)
+            ?? links.AddChild(PatientServicePermissions.PatientExternalLinks.Update, L("Permission:Update"));
+        _ = links.Children.FirstOrDefault(child => child.Name == PatientServicePermissions.PatientExternalLinks.Delete)
+            ?? links.AddChild(PatientServicePermissions.PatientExternalLinks.Delete, L("Permission:Delete"));
 
-        patientGroup.AddPermission(PatientServicePermissions.PatientLookups.Default, L("Permission:PatientLookups"));
-        patientGroup.AddPermission(PatientServicePermissions.PatientSummaries.Default, L("Permission:PatientSummaries"));
+        _ = patientGroup.GetPermissionOrNull(PatientServicePermissions.PatientLookups.Default)
+            ?? patientGroup.AddPermission(PatientServicePermissions.PatientLookups.Default, L("Permission:PatientLookups"));
+        _ = patientGroup.GetPermissionOrNull(PatientServicePermissions.PatientSummaries.Default)
+            ?? patientGroup.AddPermission(PatientServicePermissions.PatientSummaries.Default, L("Permission:PatientSummaries"));
     }
 
     private static LocalizableString L(string name)

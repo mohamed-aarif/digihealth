7de45f2838a1227fc324331dd9937cc115cf4b5e
mohamed-aarif <mohamed.aarif@live.com>
2025-12-20 22:16:35 +0400

    Restore ForwardIdentityAuthenticationForBearer import

--- src/services/configuration/ConfigurationService.HttpApi.Host/ConfigurationServiceHttpApiHostModule.cs
+++ src/services/configuration/ConfigurationService.HttpApi.Host/ConfigurationServiceHttpApiHostModule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
